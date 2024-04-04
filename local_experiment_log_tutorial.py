7eb23ad2efb9e302765c7144e31b4a48b53e4328
YasuThompson <yasutotamura6161@gmail.com>
2024-04-04 17:50:34 +0200

    Made a skelton of training with scikit learn

--- local_experiment_log_tutorial.py
+++ local_experiment_log_tutorial.py
@@ -46,7 +46,7 @@ accuracy = accuracy_score(y_test, y_pred)
 mlflow.set_tracking_uri(uri=f"http://{host}:{port}")
 
 # Create a new MLflow Experiment
-mlflow.set_experiment("MLflow Quickstart")
+mlflow.set_experiment("Ham Demo Quickstart")
 
 # Start an MLflow run
 with mlflow.start_run():
@@ -70,3 +70,17 @@ with mlflow.start_run():
         input_example=X_train,
         registered_model_name="tracking-quickstart",
     )
+
+
+# Load the model back for predictions as a generic Python Function model
+loaded_model = mlflow.pyfunc.load_model(model_info.model_uri)
+
+predictions = loaded_model.predict(X_test)
+
+iris_feature_names = datasets.load_iris().feature_names
+
+result = pd.DataFrame(X_test, columns=iris_feature_names)
+result["actual_class"] = y_test
+result["predicted_class"] = predictions
+
+result[:4]
